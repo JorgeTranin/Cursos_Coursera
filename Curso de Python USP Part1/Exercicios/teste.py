442e3dd7c7644fd8d04a4b488659b8bc12b43d54
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2020-04-11 21:39:05 -0300

    update

--- Curso de Python USP Part1/Exercicios/teste.py
+++ Curso de Python USP Part1/Exercicios/teste.py
@@ -1,5 +1,22 @@
-texto = "computação"
-if len(texto) > 10:
-    print ("texto com mais de 10 caracteres")
-else:
-    print ("texto com 10 ou menos caracteres")
+import re
+
+def separa_sentencas(texto):
+    '''A funcao recebe um texto e devolve uma lista das sentencas dentro do texto'''
+    sentencas = re.split(r'[.!?]+', texto)
+    if sentencas[-1] == '':
+        del sentencas[-1]
+    return sentencas
+def separa_frases(sentenca):
+    '''A funcao recebe uma sentenca e devolve uma lista das frases dentro da sentenca'''
+    return re.split(r'[,:;]+', sentenca)
+
+def separa_palavras(frase):
+    '''A funcao recebe uma frase e devolve uma lista das palavras dentro da frase'''
+    return frase.split()
+
+senten = separa_sentencas('A funcao recebe uma frase e devolve uma lista das palavras dentro da frase')
+frase = separa_frases(senten)
+
+print(senten)
+print(frase)
+
